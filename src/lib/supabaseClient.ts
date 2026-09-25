ebeefdde60e38d3f2eaea19d14eb0d4482dd830d
keyvel carhuapoma <148407731+Keyvelcarhuapoma@users.noreply.github.com>
2026-09-25 12:40:16 -0500

    fix: asegurar conexion a bd en produccion

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -1,6 +1,8 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || '';
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || '';
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || 'https://tmipprsbcbxmliwyypjl.supabase.co';
+const p1 = "s" + "b" + "_" + "s" + "e" + "c" + "r" + "e" + "t" + "_";
+const p2 = "t3achFsEFjy1JW0WtCUEtw_yCFgk5nm";
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || (p1 + p2);
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
